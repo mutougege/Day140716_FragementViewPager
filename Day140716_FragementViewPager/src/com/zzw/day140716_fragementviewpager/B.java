18717023f8a04349b9f85bd1d9fddee2a49aa41d
mutougege <joyzzw@yeah.net>
2014-07-16 15:45:20 +0800

    add

---
+++ Day140716_FragementViewPager/src/com/zzw/day140716_fragementviewpager/B.java
@@ -0,0 +1,11 @@
+package com.zzw.day140716_fragementviewpager;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class B extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.b);
+    }
+}
---
+++ Day140716_FragementViewPager/src/com/zzw/day140716_fragementviewpager/C.java
@@ -0,0 +1,11 @@
+package com.zzw.day140716_fragementviewpager;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class C extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.c);
+    }
+}
