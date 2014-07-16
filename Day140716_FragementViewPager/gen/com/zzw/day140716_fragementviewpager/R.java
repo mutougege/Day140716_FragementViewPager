b5dc6827b88f35728c2d7a43b3f68475030c151a
mutougege <joyzzw@yeah.net>
2014-07-16 15:42:19 +0800

    第一次实现功能

--- Day140716_FragementViewPager/gen/com/zzw/day140716_fragementviewpager/R.java
+++ Day140716_FragementViewPager/gen/com/zzw/day140716_fragementviewpager/R.java
@@ -21,13 +21,23 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int divider_normal=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080006;
+        public static final int cursor=0x7f080004;
+        public static final int linearLayout1=0x7f080000;
+        public static final int text1=0x7f080001;
+        public static final int text2=0x7f080002;
+        public static final int text3=0x7f080003;
+        public static final int viewpage=0x7f080005;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int a=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int b=0x7f030002;
+        public static final int c=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
