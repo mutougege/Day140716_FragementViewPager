c6d18385c6dd141589cec61020807dbd0332210f
mutougege <joyzzw@yeah.net>
2014-07-16 15:20:52 +0800

    1

---
+++ Day140716_FragementViewPager/gen/com/zzw/day140716_fragementviewpager/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.zzw.day140716_fragementviewpager;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
